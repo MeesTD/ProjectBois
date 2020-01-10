2cea79db2ba7dc3c5666ac6fa9295c30b1861f3f
Zeno Degenkamp <zenodegenkamp@wcw-stud-145-109-8-131.wireless.uva.nl>
2020-01-10 12:00:32 +0100

    Started randominze

---
+++ code/Algorithms/randomize.py
@@ -0,0 +1,16 @@
+import random
+
+
+def random_car(cars):
+    """"
+    Randomly picks car 
+    """
+    for car in cars: 
+        
+        
+
+def random_step(steps):
+    """
+    Randomly picks an amount of steps
+    """
+    return random.choice(steps)
