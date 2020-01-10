6861071e541aa79b4befc5547a62e2c09ac114cb
Atlas-Parasite <matspijning@gmail.com>
2020-01-10 14:40:38 +0100

    tester

--- code/Objects/rushhour.py
+++ code/Objects/rushhour.py
@@ -11,7 +11,7 @@ class RushHour():
         self.game = Board(in_file)
         self.cars = self.load_cars(in_file)
         self.current_car = None
-        self.red_car = self.choose_car(x)
+        self.red_car = self.choose_car('X')
         
     # This function will load the list of cars from the input file.
     def load_cars(self, in_file):
@@ -25,22 +25,22 @@ class RushHour():
     # This function will check wheter or not a move is actually possible
     # It checks the route a car would take with the move function and returns True
     # if it is possible and False if it is not possible.
-    def check_route(self, car, steps):
-        for i in range(steps + 1):
-            # loop over the cars and check their coords
-            for car in self.cars:
-                if self.cars[car].get_orientation:
-                    if self.cars[car].x1 or self.cars[car].x2 == 
+    # def check_route(self, car, steps):
+    #     for i in range(steps + 1):
+    #         # loop over the cars and check their coords
+    #         for car in self.cars:
+    #             if self.cars[car].get_orientation:
+    #                 if self.cars[car].x1 or self.cars[car].x2 == 
                         
-                    if self.cars[car].length == 3:
-                        self.cars[car].x3 != 
+    #                 if self.cars[car].length == 3:
+    #                     self.cars[car].x3 != 
 
-                if not self.cars[car].get_orientation:
-                    if self.cars[car].y1 ==  or self.cars[car].y2
+    #             if not self.cars[car].get_orientation:
+    #                 if self.cars[car].y1 ==  or self.cars[car].y2
 
-                    if self.cars[car].length == 3:
-                        self.cars[car].y3 != 
-        pass
+    #                 if self.cars[car].length == 3:
+    #                     self.cars[car].y3 != 
+    #     pass
 
     # This method allows a car to move and returns True if the move is possible
     # Otherwise it returns false.
@@ -51,8 +51,8 @@ class RushHour():
         self.current_car = self.choose_car(carname)
         if self.check_route(self.current_car, steps):
             if self.current_car.get_orientation() == True:
-                for coords in range(steps):
-                    if self.current_car.get_coords() + 1 != "." and 
+                # for coords in range(steps):
+                #     if self.current_car.get_coords() + 1 != "." and 
                 if direction == "L":
                     steps = steps * -1
                     self.current_car.set_coords(steps)
