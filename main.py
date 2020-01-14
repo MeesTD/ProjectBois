05f65f85ea28bac5719b2306e3a199dc49ff522c
Mees Drissen <meesdrissen@wcw-stud-145-109-38-238.wireless.uva.nl>
2020-01-14 14:15:56 +0100

    Omdat git zuur doet

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ if __name__ == "__main__":
     in_file = 'code/Data/Rushhour6x6_2.csv'
     board = board.Board(in_file)
     game = rushhour.RushHour(in_file)
-    list1 = tuple(game.cars.items())
-    print(list1)
-    print(hash(list1))
-    print(hash(list1))
+    while True:
+        
 
 
