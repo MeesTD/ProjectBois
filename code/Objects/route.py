508f3654f36b236aeb3c3ab142896d611e827786
Mees Drissen <meesdrissen@wcw-stud-145-109-38-238.wireless.uva.nl>
2020-01-15 12:17:52 +0100

    Added randomize with state remembering.

--- code/Objects/route.py
+++ code/Objects/route.py
@@ -1,5 +1,6 @@
 from . import car, rushhour, board
 import copy
+import uuid
 
 class Route():
 
@@ -14,17 +15,18 @@ class Route():
     # This method saves the current grid state to the states dictionary
     def save_state(self, car_list):
         # Try except to make sure nothing goes wrong.
-        try:
-            local_list = tuple(copy.deepcopy(car_list))
-            self.states[hash(local_list)] = local_list
-            return True
-        except:
-            return False
+        local_list = {}
+        for car in car_list.values():
+            local_list[car.name] = copy.deepcopy(car.xy)
+        self.states[uuid.uuid4().hex] = local_list
+        return True
 
     # This method returns true if a grid state exists in the states dictionary, else false.
     def get_state(self, car_list):
-        hashed_newlist = hash(tuple(car_list))
-        if self.states.get(hashed_newlist):
-            return True
-        else:
-            return False
+        check_list = {}
+        for car in car_list.values():
+            check_list[car.name] = copy.deepcopy(car.xy)
+        for check in self.states.values():
+            if check_list == check:
+                return True
+        return False
