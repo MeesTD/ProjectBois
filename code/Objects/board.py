eea610ca3efd6eb54d2d99c4aa457818bd30f280
Zeno Degenkamp <zenodegenkamp@MacBook-Air-van-Zeno.local>
2020-01-25 18:27:39 +0100

    Changed board.py

--- code/Objects/board.py
+++ code/Objects/board.py
@@ -1,44 +1,57 @@
 ###################################################################################################
-# Board.py
+# board.py
 #
-# AUTHORS
+# Zeno Degenkamp, Mats pijning, Mees drissen
 #
-# UITLEG
+# This file contains the board object
 ###################################################################################################
 import os
 import math
 
 class Board(object):
     """
-    This class will initiate a grid which consists of a matrix which consists of a list of lists.
+    The board class will initiate a grid which consists of a matrix which consists of a list of lists.
+    The board size and win_location are based on the size of the board.
     """
     
-    # Initializes a board object 
     def __init__(self, input_file):
+        """
+        Initializes a board object with use of the input file
+        """
+        
         # Board properties
         self.size = self.load_grid(input_file)
         self.grid = []
         self.win_location = self.set_winlocation(self.size)
+        
+        # Creates the y-coords based on the size of the board
         for i in range(self.size):
+            
             coordsY = i + 1
             X_axis = []
+            
+            # Creates the x-coords based on the size of the board
             for j in range(self.size):
                 
                 coordsX = j + 1
                 
+                # Creates the points with a X and a Y coord
                 totalcords = [coordsY, coordsX] 
                 
+                # Appends all the coords in totalcords
                 X_axis.append(totalcords)
+            
             self.grid.append(X_axis)
 
-    # This function sets the size based on the input file's filename. 
+  
     def load_grid(self, input_file):
         """
         Function that reads the title of the CSV file and retreives the board size
         """
+        
         size = 0
         
-        # Checks if appropriate size is in filename (Kan dynamischer?)
+        # Checks if appropriate size is in filename, if not gives error
         if "3x3" in input_file:
             size = 3
         elif "6x6" in input_file:
@@ -49,18 +62,31 @@ class Board(object):
             size = 12
         else:   
             return ("Cant't find board with given input, please try again!")
+        
         return size
 
-    # This function sets the win location of the current board.
+    
     def set_winlocation(self, size):
+        """
+        This function sets the win location of the current board
+        """
+        
         win_coords = []
+        
+        # Retrieves the coords of the win location
         winX = self.size
         winY = math.floor(size / 2) + 1
+        
+        # Creates the win coords variable
         win_coords.append(winX)
         win_coords.append(winY)
+        
         return win_coords
 
     def get_winloc(self):
+        """
+        Gets the win location of the board
+        """
         return self.win_location
 
     def __repr__(self):
